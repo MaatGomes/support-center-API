7b981c861df8dd0825a7e0e506eed417a0dcb198
Mateus Gomes <mateusgomesgo@hotmail.com>
2026-03-29 22:32:06 -0300

    feat: adding Ticket constructor

--- support-center-API/src/main/java/entities/Ticket.java
+++ support-center-API/src/main/java/entities/Ticket.java
@@ -3,13 +3,17 @@ package entities;
 import enums.Department;
 import enums.Priority;
 import enums.Status;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 
-import java.util.Date;
+import java.time.Instant;
 import java.util.UUID;
 
+@Entity
+@AllArgsConstructor
 public class Ticket {
 
     @Id
@@ -18,7 +22,7 @@ public class Ticket {
 
     private String description;
 
-    private Date initialDate;
+    private Instant initialDate;
 
     private Status status;
 
@@ -27,4 +31,14 @@ public class Ticket {
     private String originTeam;
 
     private Priority priority;
+
+
+    public Ticket (String description, Department tagDepartment, String originTeamm, Priority priority){
+        this.description = description;
+        this.initialDate = Instant.now();
+        this.status = Status.CREATED;
+        this.tagDepartment = tagDepartment;
+        this.originTeam = originTeamm;
+        this.priority = priority;
+    }
 }
